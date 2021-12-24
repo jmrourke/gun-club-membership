30d501287a541160081304cc28978e7b397ad843
James Rourke <jmrourke@yahoo.com>
2021-12-24 10:06:55 -0500

    Commit project initialization . closes #1

---
+++ src/main/java/com/rourke/gunclubmembership/GunClubMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.rourke.gunclubmembership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunClubMembershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GunClubMembershipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rourke/gunclubmembership/GunClubMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rourke.gunclubmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunClubMembershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
